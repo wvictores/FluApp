068deb8e7d8c649928d48cda056d268c151a11bd
wvictores <wvictores@gmail.com>
2017-08-16 16:03:37 -0500

    Added FluApp

---
+++ app.js
@@ -0,0 +1,37 @@
+// var url = "http://api.flutrack.org/?limit=10"
+
+// need to find location info -function getValue() var mylocation 
+
+//function fluResponse()
+//var all = document.get[0].value;
+   // $.getJSON(url);
+//console.log(data);
+
+
+
+/*$.ajax({  
+    url: 'http://api.flutrack.org/?limit=10',  
+    type: 'GET',  
+    cache: false,
+        dataType: "text",
+        success: function(data) {
+            localStorage.setItem("cache-" + type + "-" + units, data);
+            handleCache();
+        },  
+    error: function(xhr, textStatus, errorThrown) {  
+        console.log('a' + textStatus);  
+    }  
+}).done(function() {}); 
+*/
+
+angular.module("FluApp", []);
+
+angular.module("FluApp").controller("FluController", ["$scope", "$http", function MyController($scope, $http) {
+
+    $scope.items = [];
+    $scope.getItems = function() {
+        $http.get('http://api.flutrack.org/?limit=10').then(function(data) {
+                $scope.items = data;
+            })
+    };
+}]);
